dcd6f0a89337aec1ae462f62323b0f95005574ac
Soug <anglee6915@gmail.com>
2023-12-06 18:29:15 +0900

    add text,img

--- block.py
+++ block.py
@@ -12,11 +12,11 @@ class BlocksSkill(Skill):
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": "안녕하세요, 저는 MJUBOT 입니다."
+                        "text": "안녕하세요, 저는 MJUBOT 입니다.\n Your future Begins Here! MyongJi University \n 인문캠퍼스 : (03674) 서울특별시 서대문구 거북골로 34 \n 자연캠퍼스 : (17058) 경기도 용인시 처인구 명지로 116"
                     },
                     "accessory": {
                         "type": "image",
-                        "image_url": "https://i.namu.wiki/i/_Wk24M8xsAnZkawadTDB5XPa7OXGkC3YOzlSlhqMzWBuuKodwViFBzv9lPFQEGzV8Ekv5B5-hzkkVYS6n3P7kw.svg",
+                        "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAwKSDoiprrms_pHc1f9tAs_ReE2W-Kvp7aLWcs8MYQRXq8Ka-CfcBi_dIjWW89Tkx2Lc&usqp=CAU",
                         "alt_text": "opsdroid logo"
                     }
                 },
