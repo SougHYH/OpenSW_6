f5b775100d26d431e696b8061056287d55f6dc79
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 <ljy031112@gmail.com>
2023-12-07 15:44:59 +0900

    weather skill plus

---
+++ weather.py
@@ -0,0 +1,27 @@
+from opsdroid.matchers import match_regex
+
+import aiohttp
+
+
+async def get_weather(config):
+    base_url = "http://api.openweathermap.org/data/2.5/"
+    api_url = "weather?q={}&units={}&appid={}".format(
+        config['city'], config['unit'], config['api-key'])
+
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(base_url + api_url)
+        
+    return await response.json()
+
+
+@match_regex(r'(?:how|what)(?:\'s|s| is) the weather', case_sensitive=False)
+async def tell_weather(opsdroid, config, message):
+
+    weather = await get_weather(config)
+    temp = weather['main']['temp']
+    humidity = weather['main']['humidity']
+    #city = weather['name']
+    #status = weather['weather'][0]['description'].title()
+    
+    await message.respond("현재 기온은 {} 도이며, 습도는 {}% 입니다! "
+                          "for today".format(temp, humidity))
