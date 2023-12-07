65613e42e6beaee8b9282a8518e7d6a27d502a76
skap620 <c40502446@gmail.com>
2023-12-07 20:37:00 +0900

    map

--- map.py
+++ map.py
@@ -1,8 +1,23 @@
 from opsdroid.skill import Skill
 from opsdroid.matchers import match_regex
+from opsdroid.connector.slack.events import Blocks
 
-class MapLinkSkill(Skill):
-    @match_regex(r'!map')
-    async def provide_map_link(self, message):
-        map_url = 'https://www.mju.ac.kr/campusMap/mjukr/view.do?findType=B'
-        await message.respond(f'캠퍼스 맵: {map_url}')
+@match_regex(r'!map')
+async def website(opsdroid, config, message):
+    await message.respond(Blocks([
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "campus map",
+                            "emoji": True
+                        },
+                        "url": "https://www.mju.ac.kr/campusMap/mjukr/view.do?findType=B"
+                    }
+                ]
+            }
+        ]
+    ))
