108868985cfbb75ec41e626c1e17a9d24f724372
Hsbin <bubble0908@naver.com>
2023-12-07 21:28:07 +0900

    계산기모드 명령어 수정

--- calculator_skill.py
+++ calculator_skill.py
@@ -8,25 +8,25 @@ class CalculatorSkill(Skill):
         self.calculator_mode = False
         self.expression = ""
 
-    @match_regex(r'calculator')
+    @match_regex(r'!calculator')
     async def calculator_mode_handler(self, message):
         self.calculator_mode = True
-        await message.respond('Calculator mode activated. Enter a mathematical expression.')
+        await message.respond('계산기 모드가 활성화되었습니다. 계산할 수식을 입력해주세요.')
 
-    @match_regex(r'exit calculator')
+    @match_regex(r'!exit calculator')
     async def exit_calculator_handler(self, message):
         self.calculator_mode = False
         self.expression = ""
-        await message.respond('Calculator mode deactivated.')
+        await message.respond('계산기 모드가 비활성화되었습니다.')
 
     @match_regex(r'\d+([+\-*/]\d+)+')
     async def calculate_handler(self, message):
         if self.calculator_mode:
             expression = message.regex.group(0)
             result = sympy.sympify(expression)
-            await message.respond(f'Result: {result}')
+            await message.respond(f'결과: {result}')
         else:
-            await message.respond('Calculator mode not activated. Type "calculator" to activate.')
+            await message.respond('계산기 모드가 활성화 되어 있지 않습니다. 계산기 모드를 활성화해주세요.')
 
     async def default_handler(self, message):
         if self.calculator_mode:
