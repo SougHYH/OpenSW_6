137fa654bec10784c30f97850504b6d8543e827b
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 <ljy031112@gmail.com>
2023-12-07 17:45:50 +0900

    final weather.py

--- weather.py
+++ weather.py
@@ -1,27 +1,30 @@
 from opsdroid.matchers import match_regex
-
 import aiohttp
 
+# OpenWeatherMap API 토큰을 직접 코드에 설정
+API_KEY = "531a92c10601431dfddc53dd76a4695a"
+CITY = "Seoul"  # 서울로 고정
 
-async def get_weather(config):
-    base_url = "http://api.openweathermap.org/data/2.5/"
-    api_url = "weather?q={}&units={}&appid={}".format(
-        config['city'], config['unit'], config['api-key'])
+async def get_weather():
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {"q": CITY, "units": "metric", "appid": API_KEY}
 
     async with aiohttp.ClientSession() as session:
-        response = await session.get(base_url + api_url)
-        
+        response = await session.get(base_url, params=params)
+    
     return await response.json()
-
 
 @match_regex(r'!weather', case_sensitive=False)
 async def tell_weather(opsdroid, config, message):
+    weather_data = await get_weather()
 
-    weather = await get_weather(config)
-    temp = weather['main']['temp']
-    humidity = weather['main']['humidity']
-    #city = weather['name']
-    #status = weather['weather'][0]['description'].title()
-    
-    await message.respond("현재 기온은 {} 도이며, 습도는 {}% 입니다! "
-                          "for today".format(temp, humidity))
+    if "main" in weather_data and "temp" in weather_data["main"]:
+        temp = weather_data["main"]["temp"]
+        weather_status = weather_data["weather"][0]["description"].title()
+        humidity = weather_data["main"]["humidity"]
+
+        response_message = (
+            f"현재 서울의 기온은 {temp}°C 이고 "
+            f"습도는 {humidity}% 입니다."
+        )
+        await message.respond(response_message)
