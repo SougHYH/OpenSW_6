24be3aa69380b6b012d6c4886d9d81f0569274c5
Hsbin <bubble0908@naver.com>
2023-12-07 21:48:49 +0900

    계산기모드 help-> commandList에 명령어추가

--- help.py
+++ help.py
@@ -4,9 +4,11 @@ from opsdroid.matchers import match_regex
 command_list = ['MJU_BOT 에는 다음과 같은 명령을 사용 할 수 있습니다.\n',
                 '!help  -- 명령어 도움말',
                 '!intro -- 명지대 소개 및 연락처',
-                '!web   -- 명지대 공식 웹사이트'
-                '!map   -- 명지대 자연캠퍼스 지도'
-                '!menu  -- 명지대 학식 메뉴']
+                '!web   -- 명지대 공식 웹사이트',
+                '!map   -- 명지대 자연캠퍼스 지도',
+                '!menu  -- 명지대 학식 메뉴',
+                '!calculator -- 계산기 모드 활성화',
+                '!exit calculator -- 계산기 모드 비활성화',]
 
 @match_regex(r'!help')
 async def help(opsdroid, config, message):
